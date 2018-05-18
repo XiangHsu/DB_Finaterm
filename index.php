e1aaa0f3f5547ea91d63b860da6b46d378fd1d80
YU-Chih-Chieh <jackplayer00@gmail.com>
2018-05-19 00:16:32 +0800

    1

--- index.php
+++ index.php
@@ -4,7 +4,24 @@
    $password="data";//密碼
    $db="104021042"; //資料庫名稱
    $con=mysqli_connect($host, $username, $password, $db);
-   
+    $sql="SELECT name FROM `database_product`";
+    $result=mysqli_query($con,$sql);
+    $number_of_rows = mysqli_num_rows($result);
+  
+          $temp_array  = array();
+  
+          if($number_of_rows > 0) {
+                  while ($row = mysqli_fetch_assoc($result)) {
+                          $temp_array[] = $row;
+                  }
+         }else{
+			  echo "Failed";
+		  }
+    //echo json_encode(array("data"=>$temp_array));
+	$data = json_encode($temp_array);
+	echo json_encode($data);
+    mysqli_close($con);
+
 ?>
 
 <html>
@@ -12,7 +29,10 @@
 		<title>Shopping Mall</title>
 	</head>
 	<body>
-		<p>Suggestions: <span id="txtHint"></span></p>
+		<div>
+			<ul>
+				
+			</ul>
 	</body>
 	<script>
 		var data = <?php echo json_encode($json_data) ?>
